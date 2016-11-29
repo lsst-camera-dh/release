5a86265b549dd888bb29c178534fdf7173728def
Jim Chiang <jchiang@slac.stanford.edu>
2016-11-29 08:36:05 -0800

    set non-zero return code if there are errors or failures

--- tests/run_all.py
+++ tests/run_all.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+import sys
 import unittest
 
 loader = unittest.TestLoader()
 testsuite = loader.discover('.', pattern='test_*.py')
 
 runner = unittest.TextTestRunner()
-runner.run(testsuite)
+result = runner.run(testsuite)
+
+sys.exit(len(result.failures) + len(result.errors))
