5ab00f6b1d4fdd2836e2e46015ef45c5fc2058f9
Max Turri <mxturri@gmail.com>
2019-11-25 09:17:38 -0800

    Updated url to download ccs artifacts due to change to Nexus3

--- bin/install.py
+++ bin/install.py
@@ -311,8 +311,8 @@ export DATACAT_CONFIG=%s
                 # Download the CCS package only when necessary:
                 # - if it is a SNAPSHOT version
                 # - if it is a released version and it does not exist in the ccs install directory
-                base_url = "http://repo-nexus.lsst.org/nexus/service/local/artifact/maven/redirect?r=ccs-maven2-public&g=org.lsst"
-                command = 'wget --progress=dot:mega "%(base_url)s&a=%(package_name)s&v=%(package_version)s&e=zip&c=dist" -O temp.zip' % locals()
+                base_url = "http://repo-nexus.lsst.org/nexus/repository/ccs-maven2-public/org/lsst/"
+                command = 'wget --progress=dot:mega "%(base_url)s/%(package_name)s/%(package_version)s/%(package_name)s-%(package_version)s-dist.zip" -O temp.zip' % locals()
                 subprocess.call(command, shell=True, executable=self._executable)
                 if os.path.isdir(subdir):
                     subprocess.call('rm -r %(subdir)s' % locals(), shell=True, executable=self._executable)
